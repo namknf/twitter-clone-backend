8ca92b0f3331cca8622aa8482c8ed5722255a61a
anastasiya_malkina <9093264418es@gmail.com>
2021-12-19 16:22:53 +0300

    added base properties to model Tweet

--- Models/Tweet.cs
+++ Models/Tweet.cs
@@ -9,5 +9,11 @@
     public class Tweet
     {
         public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime Date { get; }
+
+        public int ProfileId { get; set; }
     }
 }
