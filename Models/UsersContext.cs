5bcec94e8ac163ff99de3a7fee292ff723e4279d
anastasiya_malkina <9093264418es@gmail.com>
2021-12-19 16:45:59 +0300

    created UsersContext for working with database

---
+++ Models/UsersContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Twitter_backend.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class UsersContext
+    {
+    }
+}
