ba5a2a12ba5f40436fe77ea8f84eefcd6b4f8cf8
anastasiya_malkina <9093264418es@gmail.com>
2021-12-19 16:07:36 +0300

    included Entity Framework

--- Models/User.cs
+++ Models/User.cs
@@ -1,11 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace twitter_backend.Models
+﻿namespace Twitter_backend.Models
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+
     public class User
     {
+        public int id { get; set; }
+
+        public string email { get; set; }
+
+        public int password { get; set; }
     }
 }
