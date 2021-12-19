f221cd6d06c0ebbd54a9a32462d26d7d71f2ca5b
anastasiya_malkina <9093264418es@gmail.com>
2021-12-19 16:49:59 +0300

    included ComponentModel.DataAnnotations, added atribute required to property Email

--- Models/User.cs
+++ Models/User.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Enter the correct E-mail")]
         public string Email { get; set; }
 
         public int Password { get; set; }
