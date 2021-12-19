72e77aca139f120f121880868a736afccaee4034
anastasiya_malkina <9093264418es@gmail.com>
2021-12-19 21:47:45 +0300

    included BCrypt

--- Models/Tweet.cs
+++ Models/Tweet.cs
@@ -5,6 +5,7 @@
     using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Threading.Tasks;
+    using BC = BCrypt.Net.BCrypt;
 
     public class Tweet
     {
--- Models/User.cs
+++ Models/User.cs
@@ -6,6 +6,7 @@
     using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.EntityFrameworkCore;
+    using BC = BCrypt.Net.BCrypt;
 
     public class User
     {
