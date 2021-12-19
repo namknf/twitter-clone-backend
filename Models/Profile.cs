6a5854855d04bfe47460e05537e149ddeced1f70
anastasiya_malkina <9093264418es@gmail.com>
2021-12-19 16:18:01 +0300

    added properties for class Profile

---
+++ Models/Profile.cs
@@ -0,0 +1,18 @@
+﻿namespace Twitter_backend.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class Profile
+    {
+        public string Name { get; set; }
+
+        public string NickName { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Date { get; }
+    }
+}
