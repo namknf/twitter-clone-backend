0bb1e7d322b99ce259dda707dd23d920b649d003
anastasiya_malkina <9093264418es@gmail.com>
2021-12-19 17:00:57 +0300

    added DataType atributes for properties with DateTime type

--- Models/Profile.cs
+++ Models/Profile.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Threading.Tasks;
 
@@ -15,6 +16,8 @@
 
         public string Description { get; set; }
 
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateProfile { get; }
 
         public User UserId { get; set; }
--- Models/Tweet.cs
+++ Models/Tweet.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Threading.Tasks;
 
@@ -11,6 +12,8 @@
 
         public string Text { get; set; }
 
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateTweet { get; }
 
         public Comment[] Comments { get; set; }
