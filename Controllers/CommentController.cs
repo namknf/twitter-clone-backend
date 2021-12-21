46e3383ed22286c78973d5ff0193a3e6f780c31f
anastasiya_malkina <9093264418es@gmail.com>
2021-12-21 11:45:49 +0300

    added field _db, constructor and method Create(Comment comment)

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -4,8 +4,23 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Twitter_backend.Models;
 
-    public class CommentController
+    public class CommentController : Controller
     {
+        private CommentsContext _db;
+
+        public CommentController(CommentsContext context)
+        {
+            _db = context;
+        }
+
+        public async Task<IActionResult> Create(Comment comment)
+        {
+            _db.Comments.Add(comment);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
     }
 }
