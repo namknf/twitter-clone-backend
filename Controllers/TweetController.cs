fa8de9ea9a87b45808d6db7efe2ffd156b45d4a0
anastasiya_malkina <9093264418es@gmail.com>
2021-12-21 11:48:04 +0300

    added field? constructor and method Creste in TweetController

--- Controllers/TweetController.cs
+++ Controllers/TweetController.cs
@@ -4,8 +4,23 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Twitter_backend.Models;
 
-    public class TweetController
+    public class TweetController : Controller
     {
+        private TweetsContext _db;
+
+        public TweetController(TweetsContext context)
+        {
+            _db = context;
+        }
+
+        public async Task<IActionResult> Create(Tweet tweet)
+        {
+            _db.Tweets.Add(tweet);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
     }
 }
