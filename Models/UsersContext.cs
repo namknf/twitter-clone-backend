1f04d0b241043686ae6064aae5ecbdf82e24d9b4
anastasiya_malkina <9093264418es@gmail.com>
2021-12-21 10:32:40 +0300

    modified UsersContext; added constructor and property

--- Models/UsersContext.cs
+++ Models/UsersContext.cs
@@ -8,5 +8,12 @@
 
     public class UsersContext : DbContext
     {
+        public UsersContext(DbContextOptions<UsersContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
     }
 }
