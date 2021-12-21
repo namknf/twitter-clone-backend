4f1cc987a19638641ad7272e4a5151112f46a4d4
anastasiya_malkina <9093264418es@gmail.com>
2021-12-21 10:57:16 +0300

    added constructor and property in ProfilesContext

--- Models/ProfilesContext.cs
+++ Models/ProfilesContext.cs
@@ -4,8 +4,16 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
 
-    public class ProfilesContext
+    public class ProfilesContext : DbContext
     {
+        public ProfilesContext(DbContextOptions<ProfilesContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Profile> Profiles { get; set; }
     }
 }
