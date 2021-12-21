403da2f082685f0b6aad4db6b223607167d6fe34
anastasiya_malkina <9093264418es@gmail.com>
2021-12-21 10:43:52 +0300

    set up UsersContext

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -6,10 +6,17 @@
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
+    using Twitter_backend.Models;
 
     [Route("api/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private UsersContext _db; // Get data context
+
+        public UsersController(UsersContext context)
+        {
+            _db = context;
+        }
     }
 }
