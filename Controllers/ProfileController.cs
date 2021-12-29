93311cd747018d4aee0ef1e98d9447119b05cd29
anastasiya_malkina <9093264418es@gmail.com>
2021-12-29 13:46:43 +0300

    added method Get(int id) in ProfileController

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -1,7 +1,9 @@
 ﻿namespace Twitter_backend.Controllers
 {
+    using System.Collections.Generic;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
     using Twitter_backend.Models;
 
     [Route("/api/profiles")]
@@ -13,6 +15,19 @@
         public ProfileController(ProfilesContext context)
         {
             _db = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Profile>>> Get(int id)
+        {
+            Profile profile = await _db.Profiles.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (profile == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(profile);
         }
 
         [HttpPost]
