860cf7def6a9440de095453c4234800e5ba512d5
anastasiya_malkina <9093264418es@gmail.com>
2022-01-01 22:25:24 +0300

    modified HashingPassword

--- Settings/HashingPassword.cs
+++ Settings/HashingPassword.cs
@@ -4,9 +4,11 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using Twitter_backend.Models;
     using BC = BCrypt.Net.BCrypt;
 
     public class HashingPassword
     {
+        private readonly User _test = new User();
     }
 }
