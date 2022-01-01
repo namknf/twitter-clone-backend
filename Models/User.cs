0a3b0282df8aa0e450850afe0efab67453077e72
anastasiya_malkina <9093264418es@gmail.com>
2022-01-01 22:15:28 +0300

    deleted model Profile and modified model User

--- Models/User.cs
+++ Models/User.cs
@@ -13,5 +13,22 @@
         [Range(8, 79, ErrorMessage = "Password must contain at least 8 characters and no more than 79")]
         [Required(ErrorMessage = "Enter the correct password")]
         public int Password { get; set; }
+
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter the correct nickname")]
+        public string NickName { get; set; }
+
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateProfile { get; }
+
+        public User[] TweetsId { get; set; }
+
+        public User[] Followers { get; set; }
+
+        public User[] Following { get; set; }
     }
 }
