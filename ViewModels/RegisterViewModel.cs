11b9f823c38fa9bcc2b762634b31926e29e33f72
anastasiya_malkina <9093264418es@gmail.com>
2022-01-14 20:39:21 +0300

    changed access modifier in properties

--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -6,21 +6,21 @@
     {
         [Required]
         [Display(Name = "Email")]
-        public string Email { get; set; }
+        public string Email { get; init; }
 
         [Required]
         [Display(Name = "Name")]
-        public string Name { get; set; }
+        public string Name { get; init; }
 
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
-        public string Password { get; set; }
+        public string Password { get; init; }
 
         [Required]
         [Compare("Password", ErrorMessage = "Passwords mismatch")]
         [DataType(DataType.Password)]
         [Display(Name = "ConfirmPassword")]
-        public string ConfirmPassword { get; set; }
+        public string ConfirmPassword { get; init; }
     }
 }
