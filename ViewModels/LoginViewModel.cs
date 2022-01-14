bc7e58102755ea3883211f3fd830220247a96502
anastasiya_malkina <9093264418es@gmail.com>
2022-01-14 20:38:34 +0300

    added properties for Authentification

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Twitter_backend.ViewModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; init; }
+
+        [Required]
+        [Display(Name = "Password")]
+        public string Password { get; init; }
+
+        [Display(Name = "Remmember?")]
+        public bool RemmemberMe { get; init; }
+
+        public string ReturnUrl { get; init; }
+    }
+}
