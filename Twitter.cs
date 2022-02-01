8e2a3085df10d05fbb591db7ac7cafc65e563255
anastasiya_malkina <9093264418es@gmail.com>
2022-02-01 13:04:30 +0300

    modified connection string, added it in main class Twitter

--- Twitter.cs
+++ Twitter.cs
@@ -2,8 +2,6 @@ namespace Twitter_backend
 {
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Identity;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
@@ -23,20 +21,17 @@ namespace Twitter_backend
         // Get connection string from configuration file
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IConfiguration>(Configuration);
-
-            string connection = Configuration.GetConnectionString("DefaultConnection");
+            var connection = Configuration.GetConnectionString("twitter_backendContextConnection");
 
             services.AddDbContext<UsersContext>(options => options.UseSqlServer(connection));
             services.AddDbContext<TweetsContext>(options => options.UseSqlServer(connection));
             services.AddDbContext<CommentsContext>(options => options.UseSqlServer(connection));
             services.AddControllers();
 
-            // services.AddSwaggerGen(c =>
-            // {
-            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Twitter_backend", Version = "v1" });
-            // });
-            // services.AddApplicationInsightsTelemetry();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Twitter_backend", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,8 +43,8 @@ namespace Twitter_backend
                 // then we display information about the error, if there is an error
                 app.UseDeveloperExceptionPage();
 
-                // app.UseSwagger();
-                // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Twitter_backend v1"));
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Twitter_backend v1"));
             }
 
             app.UseHttpsRedirection();
