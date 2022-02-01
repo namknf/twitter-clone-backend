5e11eb423b221d01d1f96b877fd185827004b181
anastasiya_malkina <9093264418es@gmail.com>
2022-02-01 21:08:46 +0300

    created helper JwtMiddleware for working with jwt tokens

---
+++ Helpers/JwtMiddleware.cs
@@ -0,0 +1,62 @@
+﻿namespace Twitter_backend.Helpers
+{
+    using System;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.IdentityModel.Tokens;
+    using Twitter_backend.Services;
+
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+
+        public JwtMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _configuration = configuration;
+        }
+
+        public async Task Invoke(HttpContext context, IUserService service)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            if (token != null)
+            {
+                AttachUserToContext(context, service, token);
+            }
+
+            await _next(context);
+        }
+
+        public void AttachUserToContext(HttpContext context, IUserService service, string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(_configuration["SecretInformation"]);
+
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero,
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                var userId = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
+
+                context.Items["User"] = service.GetById(userId);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
