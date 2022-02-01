be253a34de54180d55531d54514732e3f2fd1ab0
anastasiya_malkina <9093264418es@gmail.com>
2022-02-01 18:42:07 +0300

    created IUserService

---
+++ Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿namespace Twitter_backend.Services
+{
+    using System.Threading.Tasks;
+    using Twitter_backend.Entities;
+
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model);
+
+        Task<AuthenticateResponse> Registration(UserModel userModel);
+
+        UserModel GetById(int id);
+    }
+}
