2aa36c904ae8808d8e916661bfa9528a75c16193
anastasiya_malkina <9093264418es@gmail.com>
2022-02-02 21:25:24 +0300

    added UserRepository, that implements interface's methods

---
+++ Services/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿namespace Twitter_backend.Services
+{
+    using System.Threading.Tasks;
+    using Twitter_backend.Models;
+    using System.Linq;
+
+    public class UserRepository<T> : IAuthRegisterRepository<T> where T : Models.ModelBase
+    {
+        private readonly UsersContext _context;
+
+        public UserRepository(UsersContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Add(T entity)
+        {
+            var result = await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            return result.Entity.Id;
+        }
+
+        public T GetById(int id)
+        {
+            var gettingId = _context.Set<T>().FirstOrDefault(x => x.Id == id);
+
+            if (gettingId == null)
+            {
+                return null;
+            }
+
+            return gettingId;
+        }
+    }
+}
