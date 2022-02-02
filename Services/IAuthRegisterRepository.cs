1298a716a24f4bfd30c6b202f902b77f087c2673
anastasiya_malkina <9093264418es@gmail.com>
2022-02-02 21:24:22 +0300

    created interface IAuthRegisterRepository for class UserRepository

---
+++ Services/IAuthRegisterRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Twitter_backend.Services
+{
+    using Twitter_backend.Models;
+
+    interface IAuthRegisterRepository<T> where T: ModelBase
+    {
+        T GetById(int id);
+        Task<int> Add(T entity);
+    }
+}
