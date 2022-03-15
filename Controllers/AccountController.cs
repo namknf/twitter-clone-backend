19abcb57746f390d17b7fb0d670c33010141d614
anastasiya_malkina <9093264418es@gmail.com>
2022-03-15 15:45:31 +0300

    fixed account controller

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,11 +3,44 @@
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
+    using Twitter_backend.Entities;
     using Twitter_backend.Models;
+    using Twitter_backend.Services.Account;
 
-    [Route("api/users")]
+    [Route("api/account")]
     [ApiController]
-    public class AccountController : Controller
+    public class AccountController : ControllerBase
     {
+        private readonly IAuthService _authService;
+
+        public AccountController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate(AuthorizeRequest request)
+        {
+            var response = _authService.Authorize(request);
+
+            if (response == null)
+            {
+                return BadRequest(new {message = "Username or password are incorrect"});
+            }
+
+            return Ok();
+        }
+
+        public async Task<IActionResult> Register(User user)
+        {
+            var response = await _authService.Registration(user);
+
+            if (response == null)
+            {
+                return BadRequest(new {message = "User didn't register"});
+            }
+
+            return Ok(response);
+        }
     }
 }
