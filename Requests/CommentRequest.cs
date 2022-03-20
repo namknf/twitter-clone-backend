b71bd5b81f9d02ad09e4fc6f0599ae74fd5ea6a0
namknf <9093264418es@gmail.com>
2022-03-20 15:38:26 +0300

    modify requests: comment, tweet

---
+++ Requests/CommentRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace Twitter_backend.Requests
+{
+    using System;
+    using Twitter_backend.Models;
+
+    public class CommentRequest
+    {
+        public string Text { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        public int? UserId { get; set; }
+
+        public int? TweetId { get; set; }
+
+        public virtual Tweet Tweet { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
