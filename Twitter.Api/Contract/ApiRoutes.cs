8c88db6600bd289e36ba74aa46b7e624a06e5469
namknf <9093264418es@gmail.com>
2022-03-24 21:40:01 +0300

    add routes for comments

--- Twitter.Api/Contract/ApiRoutes.cs
+++ Twitter.Api/Contract/ApiRoutes.cs
@@ -6,11 +6,26 @@
 
         public static class Tweets
         {
-            public const string GetAll = Root + "/tweets";
-            public const string Get = Root + "/tweets/{tweetId}";
-            public const string Create = Root + "/tweets";
-            public const string Update = Root + "/tweets/{tweetId}";
-            public const string Delete = Root + "/tweets/{tweetId}";
+            public const string GetAll = Root + "tweets";
+            public const string Get = Root + "tweets/{tweetId}";
+            public const string Create = Root + "tweets";
+            public const string Update = Root + "tweets/{tweetId}";
+            public const string Delete = Root + "tweets/{tweetId}";
+        }
+
+        public static class Accounts
+        {
+            public const string Authenticate = Root + "authenticate";
+            public const string Register = Root + "registration";
+        }
+
+        public static class Comments
+        {
+            public const string GetAll = Root + "comments";
+            public const string Get = Root + "comments/{commentId}";
+            public const string Create = Root + "comments";
+            public const string Update = Root + "comments/{commentId}";
+            public const string Delete = Root + "comments/{commentsId}";
         }
     }
 }
