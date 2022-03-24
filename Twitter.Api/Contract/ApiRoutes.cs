2408bcbe4bf0cc3d8a2f015e71a778a82f3caa3c
namknf <9093264418es@gmail.com>
2022-03-24 20:40:20 +0300

    add new contract ApiRoutes with app's routes

---
+++ Twitter.Api/Contract/ApiRoutes.cs
@@ -0,0 +1,16 @@
+﻿namespace Twitter_backend.Contract
+{
+    public class ApiRoutes
+    {
+        public const string Root = "api/";
+
+        public static class Tweets
+        {
+            public const string GetAll = Root + "/tweets";
+            public const string Get = Root + "/tweets/{tweetId}";
+            public const string Create = Root + "/tweets";
+            public const string Update = Root + "/tweets/{tweetId}";
+            public const string Delete = Root + "/tweets/{tweetId}";
+        }
+    }
+}
