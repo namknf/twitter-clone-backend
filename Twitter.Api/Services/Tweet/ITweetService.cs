18b808dc0a8f6c7814750a9ee577bc9b135fa18b
namknf <9093264418es@gmail.com>
2022-03-24 20:44:25 +0300

    implement TweetService

--- Twitter.Api/Services/Tweet/ITweetService.cs
+++ Twitter.Api/Services/Tweet/ITweetService.cs
@@ -1,6 +1,20 @@
 ﻿namespace Twitter_backend.Services.Tweet
 {
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Twitter_backend.Models;
+    using Twitter_backend.Requests;
+
     internal interface ITweetService
     {
+        Task<int> Create(Tweet model);
+
+        Task<bool> Delete(Tweet model);
+
+        Tweet GetById(int id);
+
+        List<Tweet> GetAllUserTweets();
+
+        Task<bool> IsUserOwnsTweet(int userId, int tweetId);
     }
 }
--- Twitter.Api/Services/Tweet/TweetService.cs
+++ Twitter.Api/Services/Tweet/TweetService.cs
@@ -1,6 +1,49 @@
 ﻿namespace Twitter_backend.Services.Tweet
 {
-    public class TweetService
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Twitter_backend.Models;
+    using Twitter_backend.Repositories;
+    using Twitter_backend.Requests;
+
+    internal class TweetService : ITweetService
     {
+        private readonly IItemsRepository<Tweet> _itemsRepository;
+
+        public TweetService(IItemsRepository<Tweet> itemsRepository)
+        {
+            _itemsRepository = itemsRepository;
+        }
+
+        public async Task<bool> Delete(Tweet model)
+        {
+            int affected = await _itemsRepository.Remove(model);
+
+            return affected > 0;
+        }
+
+        public Tweet GetById(int id)
+        {
+            return _itemsRepository.GetById(id);
+        }
+
+        public async Task<int> Create(Tweet tweet)
+        {
+            int addedItem = await _itemsRepository.Add(tweet);
+
+            return addedItem;
+        }
+
+        public Task<bool> IsUserOwnsTweet(int userId, int tweetId)
+        {
+            var tweet = _itemsRepository.GetById(tweetId);
+
+            return Task.FromResult(tweet.UserId == userId);
+        }
+
+        public List<Tweet> GetAllUserTweets()
+        {
+            return _itemsRepository.GetAll();
+        }
     }
 }
