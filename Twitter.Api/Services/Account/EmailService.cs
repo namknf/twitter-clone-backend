b1da94908275fc9e3038bd70481b2e48a38ca856
namknf <9093264418es@gmail.com>
2022-03-26 16:07:36 +0300

    add EmailService to confirm email

---
+++ Twitter.Api/Services/Account/EmailService.cs
@@ -0,0 +1,31 @@
+﻿namespace Twitter_backend.Services.Account
+{
+    using System.Threading.Tasks;
+    using MailKit.Net.Smtp;
+    using MimeKit;
+    using Twitter_backend.Responses;
+
+    public class EmailService : IEmailService
+    {
+        public async Task SendEmailAsync(string subject, string message, AuthorizeResponse user)
+        {
+            var mimeMessage = new MimeMessage();
+
+            mimeMessage.From.Add(new MailboxAddress("Application's admin", "anastmalkina0@gmail.com"));
+            mimeMessage.To.Add(new MailboxAddress("user", user.Email));
+            mimeMessage.Subject = subject;
+
+            mimeMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message,
+            };
+
+            using var client = new SmtpClient();
+            await client.ConnectAsync(" ", 465, true);
+            await client.AuthenticateAsync(user.Email, user.Password);
+            await client.SendAsync(mimeMessage);
+
+            await client.DisconnectAsync(true);
+        }
+    }
+}
---
+++ Twitter.Api/Services/Account/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿namespace Twitter_backend.Services.Account
+{
+    using System.Threading.Tasks;
+    using Twitter_backend.Responses;
+
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string subject, string message, AuthorizeResponse user);
+    }
+}
