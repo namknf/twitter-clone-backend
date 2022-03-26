2d1e60b0424bb30d1c3cd932c76d71233e878208
namknf <9093264418es@gmail.com>
2022-03-26 16:06:44 +0300

    add property Token

--- Twitter.Api/Models/User.cs
+++ Twitter.Api/Models/User.cs
@@ -18,6 +18,8 @@ namespace Twitter_backend.Models
 
         public string Email { get; set; }
 
+        public string Token { get; set; }
+
         public string Password { get; set; }
 
         public string PasswordHash { get; set; }
