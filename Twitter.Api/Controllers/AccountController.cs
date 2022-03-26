2e25b22030595a51b0f732f05f5d147915d43a63
namknf <9093264418es@gmail.com>
2022-03-26 16:05:26 +0300

    add methods LogOut and ConfirmEmail

--- Twitter.Api/Controllers/AccountController.cs
+++ Twitter.Api/Controllers/AccountController.cs
@@ -5,6 +5,7 @@
     using Twitter_backend.Contract;
     using Twitter_backend.Models.ForMappers;
     using Twitter_backend.Requests;
+    using Twitter_backend.Responses;
     using Twitter_backend.Services.Account;
 
     [ApiController]
@@ -40,7 +41,51 @@
                 return BadRequest(new { message = "User didn't register" });
             }
 
-            return Ok(response);
+            var callbackUrl = Url.Action(
+                "ConfirmEmail",
+                "Account",
+                new { userId = user.Id, code = response.Token },
+                protocol: HttpContext.Request.Scheme);
+
+            var emailService = new EmailService();
+
+            await emailService.SendEmailAsync(
+                "Confirm your account",
+                $"Fine! You have become a member of our team. Welcome to Twitter! Confirm registration by clicking on the link: <a href='{callbackUrl}'>link</a>",
+                response);
+
+            return Content("To complete the registration, confirm your email and be sure to follow the link sent by mail");
+        }
+
+        [HttpPost(ApiRoutes.Accounts.LogOut)]
+        public async Task<IActionResult> LogOut(AuthorizeResponse response)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [HttpGet(ApiRoutes.Accounts.ConfirmEmail)]
+        public async Task<IActionResult> ConfirmEmail(int userid, string code)
+        {
+            if (code == null)
+            {
+                return BadRequest(new { message="Something went wrong..." });
+            }
+
+            var user = _authService.GetById(userid);
+
+            if (user == null)
+            {
+                return BadRequest(new { message= "No such user exists" });
+            }
+
+            var result = await _authService.ConfirmEmail(userid, code);
+
+            if (result)
+            {
+                return Ok();
+            }
+
+            return BadRequest(new { message= "User is not registered" });
         }
     }
 }
