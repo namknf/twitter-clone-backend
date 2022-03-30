8210364338c1c907d7179315e6dad1841c591fc6
namknf <9093264418es@gmail.com>
2022-03-30 19:58:19 +0300

    add validation attribute Compare

--- Twitter.Api/Models/ForMappers/RegisterModel.cs
+++ Twitter.Api/Models/ForMappers/RegisterModel.cs
@@ -1,11 +1,14 @@
 ﻿namespace Twitter_backend.Models.ForMappers
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class RegisterModel
     {
         public string Email { get; set; }
 
         public string Password { get; set; }
 
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
         public string PasswordConfirm { get; set; }
     }
 }
