771546e61f40c22e14c94739b9f574b5ad91a480
namknf <9093264418es@gmail.com>
2022-03-30 20:52:41 +0300

    add some tests

---
+++ Twitter.Tests/RepositoriesTests/AuthRepoTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Twitter.Tests.RepositoriesTests
+{
+    using NUnit.Framework;
+    using Twitter_backend.Data;
+    using Twitter_backend.Models;
+    using Twitter_backend.Repositories;
+
+    [TestFixture]
+    public class AuthRepoTests
+    {
+        private readonly AuthRegisterRepository<User> _authRepo;
+
+        public AuthRepoTests()
+        {
+            var context = new TwitterContext();
+            _authRepo = new AuthRegisterRepository<User>(context);
+        }
+
+        [Test]
+        public void CanGetUser()
+        {
+            var id = 0;
+
+            var user = _authRepo.GetById(id);
+
+            Assert.AreEqual(id, user.Id);
+        }
+    }
+}
